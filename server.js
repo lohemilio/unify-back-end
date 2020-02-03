b87847e176749dd04072eeb22dd1b3a93782b099
Emilio <a01651283@itesm.mx>
2020-02-02 19:05:17 -0600

    changes

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 require('./db/connection.js')
 const express = require ('express')
 const app = express()
-const Port = process.env.Port || 3000
+const port = process.env.PORT || 3000
 
 const router = require('./routes.js')
 
+var cors = require('cors')
 
 
+app.use(cors())
 app.use(express.json())
 //app.use('./models/universities.js',require('./routes'))
 //app.unsubscribe(router)
 
 app.use(router)
-app.listen(Port, () => console.log('Server started'))
+app.listen(port, function() {
+    console.log('Server up and running on port ' + port)
+  })
 
